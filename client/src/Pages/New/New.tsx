627040be99d91c09130880eac7fa7aaae2beac50
Cooper Doucette <coopdouce01@gmail.com>
2024-10-22 20:36:14 -0500

    New Tab CSS

--- client/src/Pages/New/New.tsx
+++ client/src/Pages/New/New.tsx
@@ -1,26 +1,22 @@
 import "./New.css";
-import { Dropdown } from "react-bootstrap";
 
 const New = () => {
     return (
         <div className="newContent">
-            <h1 className="newHeader">How Are You Feeling</h1>
-            <p className="newCard">
-                Select an emotion to log how you are feeling today.
-            </p>
-            <div className="dropdownMenu">
-            <Dropdown>
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                    Select an Emotion
-                </Dropdown.Toggle>
-
-                <Dropdown.Menu>
-                    <Dropdown.Item href="#/optionHappy">Happy</Dropdown.Item>
-                    <Dropdown.Item href="#/optionSad">Sad</Dropdown.Item>
-                    <Dropdown.Item href="#/optionAngry">Angry</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown> 
-        </div>
+            <div className="newCard">
+                <h1 className="newHeader">How Are You Feeling</h1>
+                <p>
+                    Select an emotion to log how you are feeling today.
+                </p>
+                <div className="newDropdown">
+                    <button className="newDropBtn">Dropdown</button>
+                    <div className="newDropContent">
+                        <a href="optionHappy">Happy</a>
+                        <a href="optionSad">Sad</a>
+                        <a href="optionAngry">Angry</a>
+                    </div>
+                </div>
+            </div>
         </div>
     ) 
 }
