f7a053007dbc3ea84f670d74e7234337fa06649e
Jake Walter <jakewalter080@gmail.com>
2024-10-22 19:15:12 -0500

    add function to get all available moods & log the user's input

--- server/src/controllers/moodController.ts
+++ server/src/controllers/moodController.ts
@@ -2,3 +2,21 @@
 import { Request, Response } from 'express';
 import Mood from '../models/mood.js';
 import userMood from '../models/userMood.js';
+
+export const getMoods = async (req: Request, res: Response) => {
+    try {
+        const moods = await Mood.findAll();
+        res.json(moods);
+    } catch (error) {
+        res.json({ message: `Error getting moods: ${error}` });
+    }
+};
+
+export const loguserMood = async (req: Request, res: Response) => {
+    try {
+        const { userId =, moodId = } = req.body;
+        const mood = await Mood.findByPk(moodId);
+        if (!mood) {
+            return res.json({ message: `Mood with id ${moodId} not found` });
+        }
+    }
