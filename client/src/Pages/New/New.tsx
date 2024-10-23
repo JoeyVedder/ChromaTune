4963b4624ea2c25d8288169b0ae1e96769a24db0
Cooper Doucette <coopdouce01@gmail.com>
2024-10-23 02:53:53 -0500

    More CSS updates to New Page

--- client/src/Pages/New/New.tsx
+++ client/src/Pages/New/New.tsx
@@ -1,24 +1,53 @@
+import React, { useState } from "react";
 import "./New.css";
 
-const New = () => {
-    return (
-        <div className="newContent">
-            <div className="newCard">
-                <h1 className="newHeader">How Are You Feeling</h1>
-                <p>
-                    Select an emotion to log how you are feeling today.
-                </p>
-                <div className="newDropdown">
-                    <button className="newDropBtn">Dropdown</button>
-                    <div className="newDropContent">
-                        <a href="optionHappy">Happy</a>
-                        <a href="optionSad">Sad</a>
-                        <a href="optionAngry">Angry</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    ) 
-}
+const New: React.FC = () => {
+  const [selectedEmotion, setSelectedEmotion] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (selectedEmotion) {
+      window.location.href = selectedEmotion;
+    }
+  };
+
+  return (
+    <div className="newContent">
+      <div className="newCard">
+        <h1 className="newHeader">How Are You Feeling</h1>
+        <p>Select an emotion to log how you are feeling today.</p>
+        <form className="newForm" onSubmit={handleSubmit}>
+          <div className="newDropdown">
+          <button
+            type="button"
+            className="newDropBtn"
+            onClick={() => setSelectedEmotion("")}
+          >
+            Dropdown
+          </button>
+          <div className="newDropContent">
+            <a href="#" onClick={() => setSelectedEmotion("optionHappy")}>
+              Happy
+            </a>
+            <a href="#" onClick={() => setSelectedEmotion("optionSad")}>
+              Sad
+            </a>
+            <a href="#" onClick={() => setSelectedEmotion("optionAngry")}>
+              Angry
+            </a>
+            <a href="#" onClick={() => setSelectedEmotion("optionEnergetic")}>
+              Energetic
+            </a>
+            <a href="#" onClick={() => setSelectedEmotion("optionTired")}>
+              Tired
+            </a>
+          </div>
+          </div>
+          <input type="submit" value="Submit" className="newSubmit" />
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default New;
