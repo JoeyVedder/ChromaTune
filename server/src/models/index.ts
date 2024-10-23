ce35c38d4cd0ab0927c1a98730ec70852d58f430
Jake Walter <jakewalter080@gmail.com>
2024-10-22 19:45:15 -0500

    add import for mood routes to index.ts

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,5 +1,6 @@
 import User from './User.js';
 import Mood from './mood.js';
+import moodRoutes from '../routes/moodRoutes.js';
 
 // Later you can add other models here like:
 // import Playlist from './Playlist.js';
@@ -9,6 +10,8 @@ import Mood from './mood.js';
 // For example:
 // User.hasMany(Playlist);
 // Playlist.belongsTo(User);
+
+app.use(`/api/mood`, moodRoutes);
 
 export {
     User,
