80a2e49f69ce9997c13a9af97da9e155f29b620c
Jake Walter <jakewalter080@gmail.com>
2024-10-22 20:14:59 -0500

    function to fetch playlist by mood

--- server/src/controllers/playlistController.ts
+++ server/src/controllers/playlistController.ts
@@ -2,3 +2,13 @@
 import { Request, Response } from 'express';
 import { Mood } from '../models/index.js';
 import userMood from "../models/userMood.js";
+
+export const moodController = { 
+    async (req: Request, res: Response) => {
+        try {
+            const moods = await Mood.findAll();
+            res.json(moods);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    },
