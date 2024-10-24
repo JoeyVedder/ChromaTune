a127681b62e8b8e5976e96913a09cd1cab7b938b
joey vedder <qilotog1@gmail.com>
2024-10-24 19:00:25 -0400

    added dark mode function and mood dropdown

--- client/src/Components/Header/Header.tsx
+++ client/src/Components/Header/Header.tsx
@@ -4,7 +4,11 @@ function Header() {
   return (
     <div className="headerContainer">
       <header>
+<<<<<<< HEAD
         <h1 className="headerTitle">ChromaTune</h1>
+=======
+        <h1 className="headerTitle">Welcome to ChromaTune</h1>
+>>>>>>> 7caba23 (added dark mode function and mood dropdown)
       </header>
     </div>
   );
--- client/src/Pages/New/New.tsx
+++ client/src/Pages/New/New.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./New.css";
+<<<<<<< HEAD
 
 const New: React.FC = () => {
   const [selectedEmotion, setSelectedEmotion] = useState<string>("");
@@ -56,6 +57,53 @@ const New: React.FC = () => {
       </div>
     </div>
   );
+=======
+import { useState } from "react";
+
+const New = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedEmotion, setSelectedEmotion] = useState("Moods for Tunes"); 
+
+    const emotions = ["Happy", "Sad", "Angry", "Excited", "Anxious", "Mitchell"]; //Can add more moods if needed
+
+    const toggleDropdown = () => setIsOpen(!isOpen);
+
+    const handleEmotionSelect = (emotion: any) => {
+        setSelectedEmotion(emotion);
+        setIsOpen(false); 
+    };
+
+    return (
+        <div className="newContent">
+            <h1 className="newHeader"></h1>
+            <div className="newBox"> 
+                <p className="newCard">
+                    Select a Mood for a jam session.
+                </p>
+                <div className="dropdownMenu">
+                    <div className="dropdown" onClick={toggleDropdown}>
+                        <div className="dropdownToggle">
+                            {selectedEmotion}
+                        </div>
+                        {isOpen && (
+                            <div className="dropdownOptions">
+                                {emotions.map((emotion) => (
+                                    <div 
+                                        key={emotion} 
+                                        className="dropdownOption" 
+                                        onClick={() => handleEmotionSelect(emotion)}
+                                    >
+                                        {emotion}
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+>>>>>>> 7caba23 (added dark mode function and mood dropdown)
 };
 
 export default New;
