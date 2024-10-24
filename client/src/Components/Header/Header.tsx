22425f094f4b61c4f2f75b29a7b9c965e044dd58
Cooper Doucette <coopdouce01@gmail.com>
2024-10-24 18:40:33 -0500

    added client to link in index

--- client/src/Components/Header/Header.tsx
+++ client/src/Components/Header/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className="headerContainer">
       <header>
-        <h1 className="headerTitle">Welcome to ChromaTune</h1>
+        <h1 className="headerTitle">ChromaTune</h1>
       </header>
     </div>
   );
