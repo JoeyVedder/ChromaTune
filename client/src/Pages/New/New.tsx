5dd2685bd5333f022f5f014a7097311063928497
Cooper Doucette <coopdouce01@gmail.com>
2024-10-25 02:02:43 -0500

    mood board almost working

--- client/src/Pages/New/New.tsx
+++ client/src/Pages/New/New.tsx
@@ -1,61 +1,72 @@
 import React, { useState } from "react";
 import "./New.css";
 
-const New: React.FC = () => {
-  const [selectedEmotion, setSelectedEmotion] = useState<string>("");
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (selectedEmotion) {
-      window.location.href = selectedEmotion;
-    }
+const Emotions: React.FC = () => {
+  const [mood, setMood] = useState<string>("");
+  const handleMoodChange = (selectedMood: string) => {
+    setMood(selectedMood);
   };
 
-  const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    if (selectedEmotion) {
-      window.location.hash = selectedEmotion;
+  const getBackgroundImage = (mood: string): string => {
+    switch (mood) {
+      case "optionHappy":
+        return "var(--happy)";
+      case "optionSad":
+        return "var(--sad)";
+      case "optionAngry":
+        return "var(--angry)";
+      case "optionEnergetic":
+        return "var(--energetic)";
+      case "optionTired":
+        return "var(--tired)";
+      default:
+        return "";
     }
   };
 
   return (
-    <div className="newContent">
+    <div
+      id="backgroundArea"
+      style={{backgroundImage: getBackgroundImage(mood),}}
+    >
+      <div className="newContent">
       <div className="newCard">
         <h1 className="newHeader">How Are You Feeling</h1>
         <p>Select an emotion to log how you are feeling today.</p>
-        <form className="newForm" onSubmit={handleSubmit}>
-          <div className="newDropdown">
-            <button
-              type="button"
-              id="newDropBtn"
-              className="newDropBtn"
-              onClick={handleButtonClick}
-            >
-              Dropdown
-            </button>
-            <div className="newDropContent">
-              <a href="#" onClick={() => setSelectedEmotion("optionHappy")}>
-                Happy
-              </a>
-              <a href="#" onClick={() => setSelectedEmotion("optionSad")}>
-                Sad
-              </a>
-              <a href="#" onClick={() => setSelectedEmotion("optionAngry")}>
-                Angry
-              </a>
-              <a href="#" onClick={() => setSelectedEmotion("optionEnergetic")}>
-                Energetic
-              </a>
-              <a href="#" onClick={() => setSelectedEmotion("optionTired")}>
-                Tired
-              </a>
-            </div>
+        <form className="newForm">
+        <div className="newDropdown">
+          <button
+            type="button"
+            id="newDropBtn"
+            className="newDropBtn"
+            onClick={() => handleMoodChange("")}
+          >
+            Dropdown
+          </button>
+          <div className="newDropContent">
+            <a href="#" onClick={() => handleMoodChange("optionHappy")}>
+              Happy
+            </a>
+            <a href="#" onClick={() => handleMoodChange("optionSad")}>
+              Sad
+            </a>
+            <a href="#" onClick={() => handleMoodChange("optionAngry")}>
+              Angry
+            </a>
+            <a href="#" onClick={() => handleMoodChange("optionEnergetic")}>
+              Energetic
+            </a>
+            <a href="#" onClick={() => handleMoodChange("optionTired")}>
+              Tired
+            </a>
           </div>
-          <input type="submit" value="Submit" className="newSubmit" />
+        </div>
+        <input type="submit" value="Submit" className="newSubmit" />
         </form>
       </div>
+    </div>
     </div>
   );
 };
 
-export default New;
+export default Emotions;
