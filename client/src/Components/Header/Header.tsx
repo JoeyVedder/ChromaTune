81028f0d291841e55527f6897e057f730aca4272
Jake Walter <jakewalter080@gmail.com>
2024-10-28 20:27:40 -0500

    resolved conflicts

--- client/src/Components/Header/Header.tsx
+++ client/src/Components/Header/Header.tsx
@@ -5,10 +5,14 @@ function Header() {
     <div className="headerContainer">
       <header>
 <<<<<<< HEAD
+<<<<<<< HEAD
         <h1 className="headerTitle">ChromaTune</h1>
 =======
         <h1 className="headerTitle">Welcome to ChromaTune</h1>
 >>>>>>> 7caba23 (added dark mode function and mood dropdown)
+=======
+        <h1 className="headerTitle">ChromaTune</h1>
+>>>>>>> 22425f0 (added client to link in index)
       </header>
     </div>
   );
